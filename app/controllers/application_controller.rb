a3b184b2e9e3c1cd3462d9b39571c3943d687b86
Barbara Z Johnson <umdbzj@gmail.com>
2020-01-04 14:48:54 -0600

    Change controller to display HTML instead of text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-  	render text: "Welcome to the menu randomizing program!"
+  	render html: "Welcome to the menu randomizing program!"
   end
 end
