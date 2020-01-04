ae69577749c84a8c9e03427952ef431028809705
Barbara Z Johnson <umdbzj@gmail.com>
2020-01-04 14:40:07 -0600

    Create a page for Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  def hello
+  	render text: "Welcome to the menu randomizing program!"
+  end
 end
