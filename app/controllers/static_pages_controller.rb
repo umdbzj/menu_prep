22590370e70d3e73cf0c68fa2f8a01db9bdebb21
Barbara Z Johnson <umdbzj@gmail.com>
2020-01-11 10:54:11 -0600

    Put random main dish on home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
+  	maindish_list = Maindish.all
+  	dishNum = (0..maindish_list.length-1).to_a.shuffle.first
+  	@maindish = maindish_list[dishNum]
   end
 
   def help
